9d955d01dc0b161d59e35304948fe3a70d9e9148
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2017-10-24 08:07:10 +0300

    Added rx2firebase library

--- data_firebase/src/main/java/siarhei/luskanau/iot/lamp/data/firebase/FirebaseLampRepository.java
+++ data_firebase/src/main/java/siarhei/luskanau/iot/lamp/data/firebase/FirebaseLampRepository.java
@@ -1,67 +1,41 @@
 package siarhei.luskanau.iot.lamp.data.firebase;
 
-import android.util.Log;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
+import durdinapps.rxfirebase2.RxFirebaseDatabase;
+import io.reactivex.Completable;
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
 import siarhei.luskanau.iot.lamp.domain.LampRepository;
 
 public class FirebaseLampRepository implements LampRepository {
 
-    private static final String TAG = FirebaseLampRepository.class.getSimpleName();
+    private static final String LAMP_APP_KEY = "lamp_app";
     private static final String LAMP_KEY = "lamp";
 
-    private final DatabaseReference lampDatabaseReference;
-
-    public FirebaseLampRepository() {
-        final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        lampDatabaseReference = firebaseDatabase.getReference(LAMP_KEY);
+    private static DatabaseReference getAppDatabase() {
+        return FirebaseDatabase.getInstance()
+                .getReference(LAMP_APP_KEY);
     }
 
     @Override
     public Observable<Boolean> listenLampState() {
-        return Observable.create(emitter -> lampDatabaseReference.addValueEventListener(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(final DataSnapshot dataSnapshot) {
-                if (!isDisposed(emitter, this)) {
-                    final Object valueObject = dataSnapshot.getValue();
-                    final boolean value = Boolean.valueOf(String.valueOf(valueObject));
-                    emitter.onNext(value);
-                }
-            }
-
-            @Override
-            public void onCancelled(final DatabaseError error) {
-                if (!isDisposed(emitter, this)) {
-                    emitter.onError(error.toException());
-                }
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        }));
+        return RxFirebaseDatabase
+                .observeValueEvent(
+                        getAppDatabase().child(LAMP_KEY),
+                        Boolean.class
+                )
+                .toObservable();
     }
 
     @SuppressWarnings("BooleanParameter")
     @Override
     public Observable<Void> sendLampState(final boolean lampState) {
-        return Observable.defer(() -> {
-            lampDatabaseReference.setValue(lampState);
-            return Observable.empty();
-        });
+        return Completable.create(emitter ->
+                getAppDatabase()
+                        .child(LAMP_KEY)
+                        .setValue(lampState))
+                .toObservable();
     }
 
-    @SuppressWarnings("MethodOnlyUsedFromInnerClass")
-    private boolean isDisposed(final ObservableEmitter emitter, final ValueEventListener valueEventListener) {
-        final boolean isDisposed = emitter.isDisposed();
-        if (isDisposed) {
-            lampDatabaseReference.removeEventListener(valueEventListener);
-        }
-        return isDisposed;
-    }
 }
